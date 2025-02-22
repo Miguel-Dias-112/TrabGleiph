47721c6cdbdfc61b3ee86772d996bf2185cef1d1
Gabriel Giacomo <gabrielgiacomopaes@gmail.com>
2025-02-22 20:30:59 -0300

    add to-do

---
+++ src/main/java/br/ufjf/trabfinal/Todo.java
@@ -0,0 +1,10 @@
+
+/*
+
+- verificaçoes de inputs negativos em transacoes
+- estados de persist (persit e dao) de transacao esta sobrescrevendo json
+- rever model de investimento e credito conforme requisitos trabalho
+
+
+
+*/
