e30635007ea76a9964d203aecf22a1e8899498af
Gabriel Giacomo <gabrielgiacomopaes@gmail.com>
2025-02-22 19:48:13 -0300

    estrutura projeto mvc

--- src/main/java/br/ufjf/trabfinal/Main.java
+++ src/main/java/br/ufjf/trabfinal/Main.java
@@ -1,22 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 package br.ufjf.trabfinal;
-import java.io.File;
-import java.util.List;
 
 /**
- *
- * @author MeuPawDeAsa
+ * @authors 
+ * MeuPawDeAsa
+ * ggiacz
+ * Miguel-Dias-112
  */
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        TransDAO t = new TransDAO();
-        List<Transacao> transacoes= List.of(
-               // new Transacao()
-        );
-        t.save(transacoes);
     }
 }
--- src/main/java/br/ufjf/trabfinal/model/Credito.java
+++ src/main/java/br/ufjf/trabfinal/model/Credito.java
@@ -2,15 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package br.ufjf.trabfinal;
+package br.ufjf.trabfinal.model;
 
 /**
  *
- * @author ice
+ * @author ggiacz
  */
-public class Conta{
-    private int id;
-    private String nome;
-    private String cpf;
-    private String senha;
+public class Credito {
+    
 }
