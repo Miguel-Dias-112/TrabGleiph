81df868f862fbdace261256fd0e45e5f46e942de
MiguelDias <miguel.dias@estudante.ufjf.br>
2025-03-05 21:00:38 -0300

    faz saldo ser relativo a conta

--- src/main/java/Models/Cliente.java
+++ src/main/java/Models/Cliente.java
@@ -5,7 +5,7 @@ import Utils.Exception.CadastroException;
 
 public class Cliente extends Usuario {
     private Conta conta; 
-    private double saldo;  
+    
     public Cliente(String login, String senha, String nome, String cpf) throws CPFException, CadastroException  {
         super(login, senha, nome, cpf, "Cliente");
         
@@ -19,7 +19,7 @@ public class Cliente extends Usuario {
         return conta;
     }
     public double getSaldo() {
-        this.saldo = saldo;
-        return saldo;
+        
+        return conta.getSaldo();
     }
 }
