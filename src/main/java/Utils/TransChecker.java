16a466b3a131b7a1ea53f0ecd00951c5238eb7a6
MiguelDias <miguel.dias@estudante.ufjf.br>
2025-03-06 12:37:47 -0300

    faz checagemd de uma transação usando metodo no do DAO

--- src/main/java/Utils/TransChecker.java
+++ src/main/java/Utils/TransChecker.java
@@ -3,35 +3,34 @@ package Utils;
 import Controller.DataAcessObjects.ClienteDAO;
 import Models.Cliente;
 import Utils.Exception.CPFException;
+import Utils.Exception.LoginException;
 import Utils.Exception.TransacaoException;
 
 import java.util.List;
 
 public class TransChecker {
-    public static  boolean isPasswordValid(String cpf, String senha) throws CPFException {
+    public static  boolean isPasswordValid(String cpf, String senha) throws LoginException {
         ClienteDAO clienteDAO = new ClienteDAO();
-        List<Cliente> clientes = clienteDAO.findAll();
-        for (Cliente cliente : clientes) {
-            if (cliente.getCpf().equals(cpf) && cliente.getSenha().equals(senha)) {
-                return true;
-            }
+        Cliente cliente = clienteDAO.findByCpf(cpf);
+        if (cliente.getSenha().equals(senha)) {
+            return true;
         }
-        throw new CPFException("Senha incorreta");
+        throw new LoginException();
     }
     public static  boolean isSaldoAvaible(String cpf, Double valor) throws TransacaoException {
         ClienteDAO clienteDAO = new ClienteDAO();
-        List<Cliente> clientes = clienteDAO.findAll();
-       
+        Cliente cliente = clienteDAO.findByCpf(cpf);
+        if (cliente.getSaldo() >= valor) {
+            return true;
+        }
         throw new TransacaoException("Saldo insuficiente");
     }
-    public static boolean isTransValida(String cpfOrigem, String cpDestino, Double valor, String Senha) throws CPFException, TransacaoException {
+    public static boolean isTransValida(String cpfOrigem, String cpDestino, Double valor, String Senha) throws CPFException, TransacaoException, LoginException {
         //todo
         isPasswordValid(cpfOrigem, Senha);
         CPF.isCPFCadastrado(cpfOrigem);
         CPF.isCPFCadastrado(cpDestino);
         isSaldoAvaible(cpfOrigem, valor);
- 
-
         return true;
     }
 
