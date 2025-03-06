dcc6d45e9d73a61539dd2288d4601eb2f8f2b692
MiguelDias <miguel.dias@estudante.ufjf.br>
2025-03-06 01:04:02 -0300

    converte tipo

--- src/main/java/Controller/DataAcessObjects/ClienteDao.java
+++ src/main/java/Controller/DataAcessObjects/ClienteDao.java
@@ -75,13 +75,13 @@ public class ClienteDao implements ClientePersist {
         List<Cliente> usuarios = findAll();
         for (Cliente usuario : usuarios) {
             if (usuario.getCpf().equals(user.getCpf())) {
-                int saldo = (int) usuario.getConta().getSaldo();
+                double saldo = (double) usuario.getConta().getSaldo();
                 saldo -= valor;
                 usuario.getConta().setSaldo(saldo);
                 usuario.getConta().adicionarTransacao(new Transacao(valor));
                 for (Cliente usuarioDestino : usuarios) {
                     if (usuarioDestino.getCpf().equals(cpfDestino)) {
-                        saldo = (int) usuarioDestino.getConta().getSaldo();
+                        saldo = usuarioDestino.getConta().getSaldo();
                         saldo += valor;
                         usuarioDestino.getConta().setSaldo(saldo);
                         usuarioDestino.getConta().adicionarTransacao(new Transacao(valor));
