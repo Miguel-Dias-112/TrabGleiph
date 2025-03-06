958b6aa978ba6a336b1e420c6a1c35b57b776a17
MiguelDias <miguel.dias@estudante.ufjf.br>
2025-03-06 12:22:28 -0300

    cria classe de checagem

---
+++ src/main/java/Utils/TransChecker.java
@@ -0,0 +1,21 @@
+package Utils;
+
+import Controller.DataAcessObjects.ClienteDAO;
+import Models.Cliente;
+import Utils.Exception.CPFException;
+
+import java.util.List;
+
+public class TransChecker {
+    public static boolean isTransValida(String cpfOrigem, String cpDestino, double valor, String Senha) throws CPFException {
+        //todo
+        //checar senha
+        //checar cpfs
+        //checar saldo
+
+
+        return true;
+    }
+
+    
+}
